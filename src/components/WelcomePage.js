ce155322d387f253e42d68fbf646808c0294dffb
leonardom249 <leonardo.megan@gmail.com>
2018-09-03 15:45:25 -0500

    basic welcome page set up with basic css

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
+import '../component-css/WelcomePage.css'
 
 
 
@@ -7,7 +8,22 @@ export default class WelcomePage extends Component{
 
     render(){
         return(
-            <h1>Test</h1>
+            <div className="welcome-page">
+                <div className="welcome-header">
+                    <h1>Welcome to YMTTW!</h1>
+                    <h3>YMTTW stands for something incredible:</h3>
+                    <h2>You Matter To This World</h2>
+                </div>
+                <div className="welcome-info">
+                    <p>And you truly do!  Our job is just to let you know that you're here for a reason and we are so happy that you are!</p>
+                    <p>As for us, we are a group of individuals who started to feel way too helpless watching friends, loved ones, and the world be brought down day after day.  To the point where some of us think it is better to leave this world than remain a part of it, and many of them leave because they don't realize how much they truly make an impact on others in great and fantastic ways, no matter how small or seemingly insignificant.</p>
+                    <p>Well we are here to tell you, even if you don't believe it yet, you have made a positive difference in someones life.  We want to share that and other good things through this website.  We are not doctors, we are not psychiatrists, we can not give you the most perfect advice, but we ARE friends who GENUINLEY care about every individual and the good that everyone inherintely has that goes underappreciated daily.  And we are here to change that underappreciation!</p>
+                    <p>This site will serve as a place to post good.  To show happy news, to randomly and anonymously call out people for their good acts when they didn't even realize how much that good act meant.  This site is a place to uplift us all and keep us going through this crazy thing we call life. </p>
+                </div>
+                <div className="welcome-to-come">
+                    <p><em>Thank you for taking the time to read about our upcoming site. YMTTW will be up and running Winter 2018 with lots of good and uplifting things.  Until then, if you need some uplifting moments, please checkout <a href="https://www.huffingtonpost.com/topic/good-news">Huffington Post's Good News Section</a> and always remember that if you have any suicidal thoughts or tendencies, you aren't alone in this, be strong and call <a href="https://suicidepreventionlifeline.org/">1-800-273-8255</a> or 911.  Remember <strong>You Matter To This World.</strong></em></p>
+                </div>
+            </div>
     );
     }
 }   
